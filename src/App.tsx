fa6a34b4d72c781230040be14ba94326b248a49c
linda1510 <>
2023-06-29 15:08:22 +0200

    e

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ type Message = {
 const App = () => {
   const drone = new window.Scaledrone("CQmg6PZYeclv4kdT")
 
-  // set init state for messages and users
+  // set init state for messages and userss
 
   const [messages, setMessages] = useState<Message[]>([
 
